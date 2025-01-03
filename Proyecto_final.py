529e18317130b6398b8b7d2d406fd9cc6c1019fa
Frafcis <francisco.morales.arias2710@gmail.com>
2025-01-03 18:00:21 -0300

    Add files via upload
    Fases evolutivas y tiempo de evolución logrado

--- Proyecto_final.py
+++ Proyecto_final.py
@@ -7,49 +7,8 @@ mons = {0:'Lioll_Digitama.png', 1:'Popomon.png',
         2:'Nyaromon.png',3:'Leomon.png',
         4:'Panjyamon.png', 5:'BanchoLeomon.png'}
 
-class Mon(pygame.sprite.Sprite):
-	def __init__(self, pos_x, pos_y):
-		super().__init__()
-		self.attack_animation = False
-		self.idle_sprites = []
-		
-		self.current_mon(0)
+state = 0 # 0idle 1move
 
-		self.current_sprite = 0
-		self.image = self.idle_sprites[self.current_sprite]
-
-		self.rect = self.image.get_rect()
-		self.rect.topleft = [pos_x,pos_y]
-
-	def idle(self):
-		self.animation = True
-
-	def update(self,speed):
-		if self.animation == True:
-			self.current_sprite += speed
-			if int(self.current_sprite) >= len(self.idle_sprites):
-				self.current_sprite = 0
-				self.attack_animation = False
-		if self.current_sprite == 0:
-			movement = random.randint(0,1)
-			if self.l_m != movement:
-				sprite_change(self)
-			if movement == 0 or self.rect.x <= 50:
-				self.rect.x += 1
-			elif movement == 1 or self.rect.x >= 200:
-				self.rect.x -= 1
-			self.l_m = movement
-		
-
-		self.image = self.idle_sprites[int(self.current_sprite)]
-
-	def current_mon(self,id):
-		self.current_mon = SpriteSheet(mons[id])
-		self.l_m = 1
-		fpa = 2
-		for i in range(fpa):
-			self.idle_sprites.append(self.current_mon.get_sprite(i*16,0,16,16))
-		return 0
 if __name__ == "__main__":
 	# General setup
 	pygame.init()
@@ -66,18 +25,31 @@ if __name__ == "__main__":
 	moving_sprites = pygame.sprite.Group()
 	player = Mon(100,100)
 	moving_sprites.add(player)
-
+	
 	while True:
-		player.idle()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_h]:
+			print ("Mensaje de ayuda")
+			
+		#Evolution
+		if pygame.time.get_ticks() in range(5000,7000):
+			player.hatch()
+		if pygame.time.get_ticks() in range(7000,7100):
+			player.digitize(1)
+			state = 1
 
 		# Drawing
 		screen.fill((255,255,255))
-		screen.blit(bg, (0, 0))
+		screen.blit(bg,(0, 0))
 		moving_sprites.draw(screen)
-		moving_sprites.update(0.07)
+		match state:
+			case 0:
+				moving_sprites.update(0.07,0)
+			case 1:
+				moving_sprites.update(0.07,1)
 		pygame.display.flip()
 		clock.tick(15)
--- sprites.py
+++ sprites.py
@@ -1,5 +1,59 @@
 ##Sprites
 import pygame
+import random
+
+from Proyecto_final import mons
+
+class Mon(pygame.sprite.Sprite):
+	def __init__(self, pos_x, pos_y):
+		super().__init__()
+		self.idle_sprites = []
+		self.animation = True
+		self.digitize(0)
+
+		self.current_sprite = 0
+		self.image = self.idle_sprites[self.current_sprite]
+
+		self.rect = self.image.get_rect()
+		self.rect.topleft = [pos_x,pos_y]
+
+	def update(self,speed,anim_id):
+		if self.animation == True:
+			self.current_sprite += speed
+			if int(self.current_sprite) >= len(self.idle_sprites):
+				self.current_sprite = 0
+				self.attack_animation = False
+		if self.current_sprite == 0 and anim_id == 1:
+			movement = random.randint(0,1)
+			if self.l_m != movement:
+				sprite_change(self)
+			if movement == 0 or self.rect.x <= 50:
+				self.rect.x += 1
+			elif movement == 1 or self.rect.x >= 200:
+				self.rect.x -= 1
+			self.l_m = movement
+		self.image = self.idle_sprites[int(self.current_sprite)]
+
+	def digitize(self,id):
+		self.idle_sprites = []
+		self.current_mon = SpriteSheet(mons[id])
+		self.l_m = 1
+		fpa = 2
+		for i in range(fpa):
+			self.idle_sprites.append(self.current_mon.get_sprite(i*16,0,16,16))
+		return 0
+	
+	def hatch(self,):
+		self.idle_sprites = []
+		self.idle_sprites.append(self.current_mon.get_sprite(2*16,0,16,16))
+		self.idle_sprites.append(self.current_mon.get_sprite(3*16,0,16,16))
+
+		return 0
+	def idle(self):
+		self.animation = True
+	
+	def moving(self):
+		return 0
 
 
 class SpriteSheet():
@@ -12,6 +66,8 @@ class SpriteSheet():
 		sprite.set_alpha
 		return sprite
 
+
+##Funciones
 def sprite_change(self):
 	for i in range(len(self.idle_sprites)):
 		self.idle_sprites[i] = pygame.transform.flip(self.idle_sprites[i],1,0)
