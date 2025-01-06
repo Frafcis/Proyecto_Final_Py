7b9c36295b7559c08e4423c642af6bac1596662b
Frafcis <francisco.morales.arias2710@gmail.com>
2025-01-06 01:46:37 -0300

    Add files via upload
    beta ver... espero un 4 ;-;

--- Proyecto_final.py
+++ Proyecto_final.py
@@ -2,14 +2,21 @@ import pygame, sys
 from pygame.locals import *
 
 import pandas as pd
+data_base = pd.read_csv('sleep_data_final.csv')
 
 pygame.font.init()
-# data_base = pd.read_csv('SpeedDating.csv')
+
 
 from sprites import *
+
+
 mons = {0:'Lioll_Digitama.png', 1:'Popomon.png',
         2:'Nyaromon.png',3:'Leomon.png',
         4:'Panjyamon.png', 5:'BanchoLeomon.png'}
+mons_names = {0:'Lioll_Digitama', 1:'Popomon',
+        2:'Nyaromon',3:'Leomon',
+        4:'Panjyamon', 5:'BanchoLeomon'}
+
 
 state = 0 # 0idle 1move 2evol 3move
 
@@ -29,8 +36,10 @@ TEXT_COL =(255,255,255)
 font = pygame.font.SysFont("Arialblack", 40)
 
 menu = []
-menu_items ={0:'Items',1:'Training',2:'battle',3:'alarm',4:'sleep'}
-menu_objects = ['bp.png','training.png','battle.png','alarm.png','sleep.png']
+menu_items ={0:'Items',1:'Training',2:'battle',3:'alarm',
+            4:'sleep',5:'library',6:'bath',7:'link'}
+menu_objects = ['bp.png','training.png','battle.png','alarm.png',
+                'sleep.png','library.png','bath.png','link.png']
 
 # Color de fondo personalizado
 BG_COLOR = (200, 230, 255)  # Color editable RGB
@@ -55,7 +64,7 @@ if __name__ == "__main__":
 		bg = None
 	# Creating the sprites and groups
 	moving_sprites = pygame.sprite.Group()
-	player = Mon(100,100)
+	player = Mon(100,130)
 	moving_sprites.add(player)
 	i = 0
 	j=0
@@ -75,7 +84,7 @@ if __name__ == "__main__":
 				sys.exit()
 		keys = pygame.key.get_pressed()
 		if keys[pygame.K_h]:
-			print ("Mensaje de ayuda")
+			print (enemy.attack,enemy.hp)
 			
 		#Evolution
 		state = evolution(player,state)
@@ -93,25 +102,21 @@ if __name__ == "__main__":
 						case 1:
 							print('Boton pulsado en '+ menu_items[i])
 						case 2:
-							# if player.stage == 'Baby II' or player.stage == 'Baby I':
-							# if state in [0,1] or state == None:
-							# 	break
+							if player.stage == 'Baby I' or state == 2:
+								break
 							print('Boton pulsado en '+ menu_items[i])
 							player.rect.x = 0
 							state = 3
 							enemy_sprite = pygame.sprite.Group()
-							enemy = Mon(260,100)
-							enemy.digitize(random.randint(2,5))
+							enemy = Mon(260,130)
+							enemy.digitize(random.randint(2,3))
 							enemy_sprite.add(enemy)
 						case _: break
 
-
-		# Drawing
         # Primero dibujamos todo en la superficie base
 		game_surface.fill(BG_COLOR)
 		if bg:
 			game_surface.blit(bg, (0, 0))
-
 
 		#Battle!
 		try:
@@ -123,10 +128,11 @@ if __name__ == "__main__":
 		except:
 			pass
 		if state == 3 and player.rect.x >= 110:
-			# battle
-			state = 1
+			state = battle(player,enemy,state)
+			del enemy_sprite
+		#calcular tiempo desde victoria
 
-
+		# Drawing
 		moving_sprites.draw(game_surface)
         
         # Escalar la superficie base a la pantalla
--- sprites.py
+++ sprites.py
@@ -2,9 +2,8 @@
 import pygame
 import random
 
-from Proyecto_final import mons,screen_width
-
-
+import pandas as pd
+from Proyecto_final import mons,mons_names,data_base
 
 
 class Mon(pygame.sprite.Sprite):
@@ -12,6 +11,7 @@ class Mon(pygame.sprite.Sprite):
 		super().__init__()
 		self.idle_sprites = []
 		self.animation = True
+		self.data_base = pd.read_csv('sleep_data_final.csv')
 		self.digitize(0)
 
 		self.current_sprite = 0
@@ -23,7 +23,7 @@ class Mon(pygame.sprite.Sprite):
 		#Stats!
 		self.attack = 0
 		self.hp = 0
-  
+		self.stage = 'Baby I'
 		#Health
 		self.care_mistakes = 0
 		self.hunger = 0
@@ -43,19 +43,29 @@ class Mon(pygame.sprite.Sprite):
 			elif movement == 1 or self.rect.x >= 200:
 				self.rect.x -= 2
 			self.l_m = movement
-		if anim_id == 3:
-			entrance(self,1)
-		if anim_id == 4:
-			entrance(self,-1)
+		elif anim_id == 3:
+			entrance(self,1) #izquierda
+		elif anim_id == 4:
+			entrance(self,-1) #derecha
+		elif anim_id == 5:
+			victory(self)
+		elif anim_id ==6:
+			defeat(self)
 		self.image = self.idle_sprites[int(self.current_sprite)]
 
 	def digitize(self,id):
 		self.idle_sprites = []
+		self.current_stats = None
 		self.current_mon = SpriteSheet(mons[id])
+		self.current_stats = data_base.loc[data_base['name'] == mons_names[id]]
 		self.l_m = 1 #1derecha #0izquierda
 		fpa = 2
 		for i in range(fpa):
 			self.idle_sprites.append(self.current_mon.get_sprite(i*16,0,16,16))
+		if id >= 2:
+			self.attack = self.current_stats['attack'].item()
+			self.hp = self.current_stats['hp'].item()
+			self.stage = self.current_stats['stage'].item()
 		return 0
 	
 	def hatch(self):
@@ -70,21 +80,21 @@ class Mon(pygame.sprite.Sprite):
 
 
 class SpriteSheet():
-    def __init__(self, file):
-        self.sheet = pygame.image.load(file).convert_alpha()
-    
-    def get_sprite(self, x, y, w, h, scale=1):
+	def __init__(self, file):
+		self.sheet = pygame.image.load(file).convert_alpha()
+		self.file_name = file
+	def get_sprite(self, x, y, w, h, scale=1):
         # Crear superficie con canal alpha
-        sprite = pygame.Surface([w, h], pygame.SRCALPHA)
-        sprite.blit(self.sheet, (0, 0), (x, y, w, h))
+		sprite = pygame.Surface([w, h], pygame.SRCALPHA)
+		sprite.blit(self.sheet, (0, 0), (x, y, w, h))
         
-        if scale != 1:
+		if scale != 1:
             # Escalar el sprite al tamaño deseado
-            scaled_w = w * scale
-            scaled_h = h * scale
-            sprite = pygame.transform.scale(sprite, (scaled_w, scaled_h))
+			scaled_w = w * scale
+			scaled_h = h * scale
+			sprite = pygame.transform.scale(sprite, (scaled_w, scaled_h))
         
-        return sprite
+		return sprite
 
 class Menu(pygame.sprite.Sprite):
 	def __init__(self,id,i,j):
@@ -117,18 +127,64 @@ def evolution(player,state):
 		player.hatch()
 		state = 2
 	if pygame.time.get_ticks() in range(9001,9100):
-		player.digitize(1)
+		player.digitize(3)
 		state = 1
-	if pygame.time.get_ticks() in range(15000,19000):
-		player.hatch()
-		state = 2
-	if pygame.time.get_ticks() in range(19001,20000):
-		player.digitize(2)
-		state = 1
+	# if pygame.time.get_ticks() in range(15000,19000):
+	# 	player.hatch()
+	# 	state = 2
+	# if pygame.time.get_ticks() in range(19001,20000):
+	# 	player.digitize(2)
+	# 	state = 1
+	# if pygame.time.get_ticks() in range(30000,34000):
+	# 	player.hatch()
+	# 	state = 2
+	# if pygame.time.get_ticks() in range(34001,34100):
+	# 	player.digitize(3)
+	# 	state = 1
+	# if pygame.time.get_ticks() in range(50000,54000):
+	# 	player.hatch()
+	# 	state = 2
+	# if pygame.time.get_ticks() in range(54001,54100):
+	# 	player.digitize(4)
+	# if pygame.time.get_ticks() in range(70000,74000):
+	# 	player.hatch()
+	# 	state = 2
+	# if pygame.time.get_ticks() in range(74001,74100):
+	# 	player.digitize(5)
+	# 	state = 1
 	return state
 
+def battle(player,enemy,state):
+	rng_player = random.randint(1,5)
+	rng_enemy = random.randint(1,5)
+	enemy_attack = rng_enemy*enemy.attack
+	player_attack = rng_player*player.attack
+	player_hp = player.hp
+	enemy_hp = enemy.hp
+	while player_hp > 0 and enemy_hp > 0:
+		if player_attack >= enemy_attack:
+			enemy_hp -= enemy_hp+(int(player_attack/10))
+		elif player_attack < enemy_attack:
+			player_hp -= player_hp+(int(enemy_attack/10))
+	
+	if enemy_hp <= 0:
+		return 5
+	elif player_hp <= 0:
+		return 6
+
+#Movement
 def entrance(self,dir):
 	if self.l_m > 1:
 		sprite_change(self)
 		self.l_m = 1
 	self.rect.x += 1*dir
+
+def victory(self):
+	self.idle_sprites = []
+	self.idle_sprites.append(self.current_mon.get_sprite(0*16,2*16,16,16))
+	self.idle_sprites.append(self.current_mon.get_sprite(1*16,2*16,16,16))
+ 
+def defeat(self):
+	self.idle_sprites = []
+	self.idle_sprites.append(self.current_mon.get_sprite(0*16,3*16,16,16))
+	self.idle_sprites.append(self.current_mon.get_sprite(1*16,3*16,16,16))
